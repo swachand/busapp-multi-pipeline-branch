f3be98c7bea38542befa87259f5f766cdd49d7b4
Swachand Jambhulkar <swachandj@gmail.com>
2026-03-22 19:54:11 +0530

    Change deployment environment from staging to qa

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-version=staging
-env="staging"
+version=qa
+env="qa"
 
 if docker ps -a --format '{{.Names}}' | grep "${env}"
 then
 docker stop ${env} &&  docker rm ${env}
 fi
-docker run -it -d -p 8081:8001 --name ${env} swach/busapp:${version}
+docker run -it -d -p 8082:8001 --name ${env} swach/busapp:${version}
