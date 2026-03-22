d973b75825a243511a173748288a5db148f787e3
root <root@ip-172-31-25-228.ec2.internal>
2026-03-22 10:41:28 +0000

    updating

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -6,4 +6,4 @@ env="develop"
 if  docker ps -a --format '{{.Names}}' | grep "${env}"
 then docker stop ${env} &&  docker rm ${env}
 fi
-docker run -it -d -p 7000:7001 --name ${env} swach/busapp:${version}
+docker run -it -d -p 7000:8001 --name ${env} swach/busapp:${version}
