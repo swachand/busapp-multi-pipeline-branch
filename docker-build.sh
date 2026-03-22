021ff776861ac2853f8bb5f3d82ac0543acbd9d3
root <root@ip-172-31-25-228.ec2.internal>
2026-03-22 04:21:12 +0000

    Removed sudo from build script

--- docker-build.sh
+++ docker-build.sh
@@ -2,5 +2,5 @@
 
 version=development
 
-sudo docker build -t learnitguide/busapp:${version} .
-sudo docker push learnitguide/busapp:${version}
+docker build -t learnitguide/busapp:${version} .
+docker push learnitguide/busapp:${version}
