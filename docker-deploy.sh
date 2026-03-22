b313b3e0795b3e292b5c9eb5882e7d326159a9b0
root <root@ip-172-31-25-228.ec2.internal>
2026-03-22 04:23:22 +0000

    Removed sudo from build script

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-version=development
-env="development"
+version=master
+env="master"
 
-if sudo docker ps -a --format '{{.Names}}' | grep "${env}"
+if docker ps -a --format '{{.Names}}' | grep "${env}"
 then
-sudo docker stop ${env} && sudo docker rm ${env}
+docker stop ${env} && docker rm ${env}
 fi
-sudo docker run -it -d -p 8000:8001 --name ${env} learnitguide/busapp:${version}
+docker run -it -d -p 8000:8001 --name ${env} learnitguide/busapp:${version}
