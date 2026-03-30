7bffd3614f43a990027eda70aff2f53171fac4f7
root <root@ip-172-31-77-228.ec2.internal>
2026-03-30 16:47:27 +0000

    updating master

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-version=development
-env="development"
+env=$BRANCH_NAME
 
-if docker ps -a --format '{{.Names}}' | grep "${env}"
+echo "Deploying for environment: $env"
+
+# Stop & remove existing container
+if docker ps -a --format '{{.Names}}' | grep -w "$env"
 then
-docker stop ${env} && docker rm ${env}
+  docker stop $env && docker rm $env
 fi
-docker run -it -d -p 8000:8001 --name ${env} swach/busapp:$BRANCH_NAME
+
+# Run new container
+docker run -d -p 8000:8001 --name $env swach/busapp:$env
